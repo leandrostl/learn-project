d3302efde6bc0ea83fcf49c33776160c7830e9d4
Leandro Lima <leandro.stlima@gmail.com>
2021-07-09 14:58:27 -0300

    Feature/leandro/better api doc (#2)
    * Add idea folder as https://intellij-support.jetbrains.com/hc/en-us/articles/206544839 article suggests
    
    * Make better docs to api.

--- app/src/main/kotlin/com/leandro/learnproject/HelloController.kt
+++ app/src/main/kotlin/com/leandro/learnproject/HelloController.kt
@@ -3,9 +3,35 @@ package com.leandro.learnproject
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.reactivex.Single
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 
 @Controller("/hello")
 class HelloController {
+    /**
+     * @return Hello World!
+     */
     @Get(produces = [MediaType.TEXT_PLAIN])
-    fun index(): String = "Hello World"
+    fun helloWorld(): String = "Hello World"
+
+    /**
+     * Shows a message of greeting to given name.
+     *
+     * @param name The person's name
+     * @return The greeting message
+     */
+    @Get(uri = "/{name}", produces = [MediaType.TEXT_PLAIN])
+    @Operation(
+        summary = "Greets a person", description = "A friendly greeting is returned", responses = [
+            ApiResponse(content = [Content(mediaType = "text/plain", schema = Schema(type = "string"))]),
+            ApiResponse(responseCode = "400", description = "Invalid Name Supplied"),
+            ApiResponse(responseCode = "404", description = "Person not found")
+        ]
+    )
+    @Tag(name = "greeting")
+    fun greetings(name: String) = Single.just("Hello $name!")
 }
