b2799229f2de4968b69aa0118f6b9816219fae94
Leandro Lima <leandro.stlima@gmail.com>
2021-07-14 20:53:30 -0300

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ kapt {
 
 jacoco {
     toolVersion = "0.8.7"
-    reportsDirectory.set(layout.buildDirectory.dir("customJacocoReportDir"))
+    reportsDirectory.set(layout.buildDirectory.dir("$buildDir/reports/jacoco"))
 }
 
 tasks {
@@ -95,9 +95,15 @@ tasks {
     }
     jacocoTestReport {
         reports {
-            xml.required.set(false)
-            csv.required.set(false)
-            html.outputLocation.set(layout.buildDirectory.dir("jacoco"))
+            xml.required.set(true)
+            html.outputLocation.set(layout.buildDirectory.dir("$buildDir/reports/jacocoHtml"))
         }
     }
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
 }
