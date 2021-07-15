c498b765d3136ceffc17a4f7e92e50460b536f82
Leandro Lima <leandro.stlima@gmail.com>
2021-07-14 22:15:03 -0300

    Change tests considering security applied.

--- app/src/test/kotlin/com/leandro/learnproject/HelloClientSpec.kt
+++ app/src/test/kotlin/com/leandro/learnproject/HelloClientSpec.kt
@@ -3,6 +3,7 @@ package com.leandro.learnproject
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.util.Base64
 import javax.inject.Inject
 
 @MicronautTest
@@ -11,6 +12,8 @@ internal class HelloClientSpec @Inject constructor(
 ) {
     @Test
     fun `should retrieve Hello World`() {
-        assertEquals("Hello World", client.hello().blockingGet())
+        val credentials: String = Base64.getEncoder().encodeToString("leandro:sneezy".toByteArray(Charsets.UTF_8))
+        val blockingGet = client.hello("Basic $credentials").blockingGet()
+        assertEquals("Hello World", blockingGet)
     }
 }
